0b89c3cdaf6e31173b2a82dba9b2d3ed0a06e79c
thiago ochoa <ochoathigo454@gmail.com>
2025-02-17 09:19:02 -0300

    aula sobre estrutura e codigos basicos em java

---
+++ aula01/Numeros.java
@@ -0,0 +1,11 @@
+public class Numeros {
+  public static void main(String[] args) {
+    System.out.println(3 + 4);
+    System.out.println(5 * 9);
+    System.out.println(5 / 54);
+    System.out.println(8 - 90);
+    
+    }
+}
+
+        
