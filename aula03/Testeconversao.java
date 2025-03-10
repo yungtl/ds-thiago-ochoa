e9872569225100437e1e77cf030415c4cc0546c8
thiago ochoa <ochoathiago454@gmail.com>
2025-03-10 09:22:36 -0300

    regras de tipagem e input

---
+++ aula03/Testeconversao.java
@@ -0,0 +1,27 @@
+public class Testeconversao {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+
+
+    
+    double myedouble = 9.78d;
+    int mytint = (int) myDouble; // Explicit casting: double to int
+
+    System.out.println(mye
+    double);
+    System.out.println(mytint);
+
+
+
+
+        double myDiouble = 9.78d;
+    int myInit = (int) myDiouble; // Explicit casting: double to int
+
+    System.out.println(myDiouble);
+    System.out.println(myInit);
+  }
+}
---
+++ aula03/Tiposvariaveis.java
@@ -0,0 +1,20 @@
+public class Tiposvariaveis{
+  public static void main(String[] args) {
+    int myNum = 5;               // integer (whole number)
+    float myFloatNum = 5.99f;    // floating point number
+    char myLetter = 'D';         // character
+    boolean myBool = true;       // boolean
+    String myText = "Hello";     // String    
+    System.out.println(myNum);
+    System.out.println(myFloatNum);
+    System.out.println(myLetter);
+    System.out.println(myBool);
+    System.out.println(myText);
+  
+      double d1 = 12E4d;
+    System.out.println(d1);  
+}
+}
+
+
+
