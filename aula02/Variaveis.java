943e0e176b92287656d16d4d1ac6ef11cdc95663
thiago ochoa <ochoathiago454@gmail.com>
2025-03-17 09:30:14 -0300

    clear

---
+++ aula02/Variaveis.java
@@ -0,0 +1,31 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        String name = "John";
+        System.out.println(name);
+       
+        int n1 = 4; 
+        int n2 = 5; 
+        System.out.println(n1);
+        System.out.println(n1 + n2);
+
+        String msg = "Ola tudo bom?";
+        System.out.println(msg);
+
+        float f1 = (float) 12.50;
+        float f2 = (float) 18.90;
+        System.out.println(f1);
+        System.out.println(f1 + f2);
+
+        char ch1 = 'a'; 
+        char ch2 = 'B';
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+
+    }
+    }
---
+++ aula04/Condicao.java
@@ -0,0 +1,17 @@
+public class Condicao.java
+{ public static void main(String [] args) 
+    {
+int V1 = 17 ;
+int V2 = 18 ;
+if (V1 > V2)
+ {
+system.out.println(V1 + " e maior que " + V2 );
+} else {
+system.out.println(V1 + " e menor que " + V2 );
+     } 
+   }
+ }
+
+
+ 
+
